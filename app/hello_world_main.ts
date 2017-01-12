890d4121cbf1d857a11680794df0b8d9742f6ca5
Sahil Jain <sahilx@live.com>
2017-01-12 17:50:49 +0530

    Continue the tutorial

--- app/hello_world_main.ts
+++ app/hello_world_main.ts
@@ -1,4 +1,4 @@
-import {bootstrap} from "angular2/platform/browser"
-import {MyHelloWorldClass} from "./hello_world_app.component"
+import {bootstrap} from "angular2/platform/browser" //importing bootstrap function
+import {MyHelloWorldClass} from "./hello_world_app.component"   //importing component function
 
 bootstrap(MyHelloWorldClass);
