a77d368bf8d948593c36ced681ea9edd6d752757
Sahil Jain <sahilx@live.com>
2017-01-19 11:37:44 +0530

    Restructured Files

--- app/my-component-main.ts
+++ app/my-component-main.ts
@@ -1,4 +1,4 @@
 import {bootstrap} from "angular2/platform/browser";
 import {MyComponentComponent} from "./my-component.component"
 
-bootstrap (MyComponentComponent)
+bootstrap (MyComponentComponent) 
